728abf1e6d0031c011557980e5b65ac0a6d9265e
Sergio Carracedo <info@sergiocarracedo.es>
2017-04-12 13:27:25 +0200

    Component for jsvigo

---
+++ app/src/renderer/components/UserGroup/JSVigo/JSVigo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="jsvigo">
+        <obs-header
+                :title="meetup.title"
+                :logo="true"
+        ></obs-header>
+
+        <obs-aside :isLightning="currentTalk.isLightning"></obs-aside>
+
+        <obs-footer :speaker="currentTalk.speaker" :title="currentTalk.title"></obs-footer>
+    </div>
+</template>
+
+<script>
+    import * as types from '../../../vuex/mutation-types'
+    import UserGroup from '../UserGroup';
+    export default {
+        name: 'jsvigo',
+        mixins: [UserGroup],
+    }
+</script>
+
+<style src="./assets/fonts/opensans.css" scoped></style>
+<style src="./assets/css/jsvigo.css"></style>
